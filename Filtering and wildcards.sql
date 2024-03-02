87942dae6c476073189d0cf5b2ddfa45ffc99e7e
Alaa Osama <83586424+alaaosama26@users.noreply.github.com>
2024-03-02 11:53:52 +0200

    Update Filtering and wildcards.sql

--- Filtering and wildcards.sql
+++ Filtering and wildcards.sql
@@ -100,3 +100,32 @@ SELECT *
 FROM Suppliers
 where contactname LIKE '%Guy%'
 and country = 'Canada'
+
+-- Q1: What is the category ID for Meat & Poultry?
+SELECT categoryid 
+FROM Categories
+WHERE categoryname = "Meat/Poultry"
+
+-- Q2: What is the name of the most expensive product that it out of stock in the “Meat” category ?
+select productname
+from Products
+where categoryid=6
+and unitsinstock = 0
+
+-- Q3: For this product in the previous question , what are the order IDs when it was sold with 25% discount with quantity over 50?
+select orderid
+from "Order Details"
+where productid = 29
+and discount = 0.25
+and quantity >= 50
+
+-- Q4: What is the most common title for the employees that are based in USA or UK and their title of courtesy are "Dr."?
+select title
+from Employees
+where country in ("USA", "UK") and titleofcourtesy = "Dr."
+  
+-- Q5: Some shipments returned as the address wasn't clear. It was being shipped to a country inside America and to a street have "rue" in it's name. 
+SELECT *
+from Customers
+where address LIKE "%rue%"
+and region LIKE "%America"
